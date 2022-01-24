7a279b11cba96eea80597a5e24528d631181c706
lari-ssa23 <larissa.isabel@unesp.br>
2022-01-23 22:41:45 -0300

    contagens no js

---
+++ script.js
@@ -0,0 +1,28 @@
+//exercio 1
+for(let i=1; i<=10;i+=1){
+    console.log(i)
+    }
+    
+    //exercicio 2
+    i=10
+    while (i>=1){
+    console.log(i)
+    i-=1
+    }
+    
+    
+    //exercicio 3
+    i=1
+    while (i<=100){
+    console.log(i)
+    i+=2
+    }
+    
+    //exercicio 4
+    i=0
+    while (i<=100){
+    console.log(i)
+    i+=2
+    }
+    
+
